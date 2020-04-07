34eb05a83195c84ec05c35e4508e20e79f706beb
Na, Zhenye <nazhenye@gmail.com>
2020-04-07 15:47:57 -0700

    deploy to zeit now

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -7,6 +7,7 @@
 
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
+const path = require('path');
 
 // List of projects/orgs using your project for the users page.
 const users = [
@@ -21,6 +22,7 @@ const users = [
 ];
 
 const siteConfig = {
+  customDocsPath: path.basename(__dirname) + '/docs',
   title: "Test Site", // Title for your website.
   tagline: "A website for testing",
   url: "https://zhenye-na.github.io", // Your website URL
