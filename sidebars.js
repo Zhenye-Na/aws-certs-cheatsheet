edf7e805d82a9edce547136fd4cb1e7b44df0e81
Na, Zhenye <nazhenye@gmail.com>
2020-04-08 23:13:16 -0700

    Update sidebar index

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  Sidebar: {
+    Introduction: ["intro"],
+    "Solution Architect - Associate": ["csa/chapter0", "csa/chapter1"],
+    Others: ["doc1"]
+  }
 };
