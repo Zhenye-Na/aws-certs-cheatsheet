382d39d31812b1dd621869d65dd139fcecf1997c
Na, Zhenye <nazhenye@gmail.com>
2020-04-19 23:08:50 -0700

    Update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,11 @@
 module.exports = {
   Sidebar: {
     Introduction: ["intro"],
-    "Solution Architect - Associate": ["csa/chapter0", "csa/chapter1"],
+    "Solution Architect - Associate": [
+      "csa/chapter0",
+      "csa/chapter1",
+      "csa/chapter2",
+    ],
     Others: ["doc1"]
   }
 };
